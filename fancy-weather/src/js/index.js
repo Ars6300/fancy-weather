df68f07b85bba0b07893552590e0624099f4899b
ars6300 <ars6300@live.com>
2020-05-31 16:21:03 +0500

    feat: add loader image and search functional

--- fancy-weather/src/js/index.js
+++ fancy-weather/src/js/index.js
@@ -5,16 +5,38 @@ import { getWeatherForecast, getCurrentWeather } from './handler';
 import { putTodayWeather, putForecastWeather } from './render';
 import setMap from './map';
 
-document.addEventListener('DOMContentLoaded', () => {
-  getLocation().then((res) => {
-    getCurrentWeather(res.city, 'en').then((ans) => {
-      if (typeof ans === 'string') {
-        alert(ans);
-      } else {
-        putTodayWeather(ans);
-        setMap(ans.lat, ans.lon);
-      }
-    });
-    getWeatherForecast(res.city, 'en').then((ans) => putForecastWeather(ans));
+const loader = document.querySelector('body > img');
+const searchForm = document.querySelector('body > main > div.search-block > form');
+const input = document.querySelector('body > main > div.search-block > form > input[type=search]');
+
+async function setWeatherByCity(city) {
+  await getCurrentWeather(city, 'en').then((ans) => {
+    if (typeof ans === 'string') {
+      alert(ans);
+    } else {
+      putTodayWeather(ans);
+      setMap(ans.lat, ans.lon);
+    }
   });
+  await getWeatherForecast(city, 'en').then((answer) => {
+    if (typeof answer === 'string') {
+      alert(answer);
+    } else {
+      putForecastWeather(answer, 'en');
+    }
+  });
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  if (input.value.length > 0) {
+    loader.classList.remove('hidden');
+    setWeatherByCity(input.value).then(loader.classList.add('hidden'));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loader.classList.remove('hidden');
+  getLocation().then((res) => setWeatherByCity(res.city).then(loader.classList.add('hidden')));
+  searchForm.addEventListener('submit', (e) => submitForm(e));
 });
